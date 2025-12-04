0fcbb3103a60eb8ac10bf717358694b14de4e178
Roushan kumar <roushankumar@Roushans-MacBook-Air.local>
2025-12-04 22:49:16 +0530

    chore: Remove screenshot files and update .gitignore

--- TutorTrek-master/client/src/api/endpoints/auth/auth.ts
+++
@@ -1,7 +0,0 @@
-import END_POINTS from "../../../constants/endpoints";
-import { login } from "../../services/auth/auth-service";
-import { AdminLoginInfo } from "../../types/admin/auth-interface";
-
-export const loginAdmin = (adminInfo:AdminLoginInfo)=>{
-    return login(END_POINTS.LOGIN_ADMIN,adminInfo)
-}
--- TutorTrek-master/server/src/types/adminAuthInterface.ts
+++
@@ -1,8 +0,0 @@
-export interface AdminLoginInfo {
-    email:string;
-    password:string;
-}
-
-export interface AdminSavedDbInterface extends AdminLoginInfo{
-    _id:string;
-}
